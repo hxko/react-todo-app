353317f9e744ca78d28e47e70a47d29d0fd413c6
hxko <heikosohn@gmail.com>
2025-06-18 15:18:44 +0200

    FEATURE: Delete All

---
+++ src/components/DeleteAllTodos.tsx
@@ -0,0 +1,21 @@
+// DeleteAllTodos.tsx
+import React from 'react';
+import { useTodoContext } from '../context/TodoContext';
+import { RiDeleteBinLine } from 'react-icons/ri';
+const DeleteAllTodos: React.FC = () => {
+  const { setTodos } = useTodoContext(); // Assuming you have a method to set todos in your context
+
+  const handleDeleteAll = () => {
+    if (window.confirm("Are you sure you want to delete all todos?")) {
+      setTodos([]); // Reset the todos to an empty array
+    }
+  };
+
+  return (
+    <button className="btn" onClick={handleDeleteAll} style={{ marginRight: '1rem', marginLeft: '1rem' }}>
+      <RiDeleteBinLine /> Delete All
+    </button>
+  );
+};
+
+export default DeleteAllTodos;
